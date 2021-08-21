bbd04f376391e58f19cc76e3a62fc17346b1bc13
NamagembeDaphine <namagembedaphine@gmail.com>
2021-08-21 16:14:59 +0300

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,16 @@
+from flask Import flask
+import urllib.request
+import numpy as np
+
+app = Flask(__name__)
+
+url = input (' Please Enter Image URL: ')
+
+@app.route('/')
+def display_image():
+    url_response = urllib.request.urlopen(url)
+    img = np.array(bytearray(url_response.read()), dtype=np.uint8)
+    return img
+
+if __name__ == '__main__':
+     app.run(host="0.0.0.0", debug=True)
